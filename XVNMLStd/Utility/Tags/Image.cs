c6781f348613665d11abe76684a57c7aa14789f5
Tyrease Teer <kayaneseera@gmail.com>
2023-04-28 23:02:23 -0700

    Added HanaHappy.png For Testing
    Testing Image and Audio tags getting relative paths.

--- XVNMLStd/Utility/Tags/Image.cs
+++ XVNMLStd/Utility/Tags/Image.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using XVNML.Core.IO.Enums;
 using XVNML.Core.Tags;
 
 namespace XVNML.XVNMLUtility.Tags
@@ -6,24 +8,39 @@ namespace XVNML.XVNMLUtility.Tags
     [AssociateWithTag("image", typeof(ImageDefinitions), TagOccurance.Multiple)]
     public sealed class Image : TagBase
     {
-        const string _CastDir = @"\Images\";
+        internal DirectoryRelativity relativity;
         internal DirectoryInfo? dirInfo;
+        internal byte[] data;
 
         public override void OnResolve(string? fileOrigin)
         {
             AllowedParameters = new[]
             {
+                "rel",
                 "src"
             };
 
             base.OnResolve(fileOrigin);
+            var rel = GetParameterValue("rel");
             string src = (string?)GetParameterValue("src") ?? string.Empty;
-            dirInfo = new DirectoryInfo(fileOrigin + _CastDir + src);
+            relativity = rel == null ? default : (DirectoryRelativity)Enum.Parse(typeof(DirectoryRelativity), rel.ToString()!);
+            var pathFlow = relativity == DirectoryRelativity.Relative ? fileOrigin + @"\" + "Images\\" + src : src;
+            //var pathFlow = relativity == DirectoryRelativity.Relative ? fileOrigin + @"\" + XVNMLConfig.RelativePath["Image"] + src : src;
+            if (pathFlow == string.Empty) return;
+            dirInfo = new DirectoryInfo(pathFlow);
+            ProcessData();
         }
 
         public override string ToString()
         {
             return dirInfo == null ? string.Empty : dirInfo!.FullName;
+        }
+
+        private void ProcessData()
+        {
+            if (dirInfo == null) return;
+            if (File.Exists(dirInfo.FullName) == false) return;
+            data = File.ReadAllBytes(dirInfo.FullName);
         }
     }
 }
